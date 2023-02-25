cc9e3697cf98f147eda55e3d065609b1fc434395
kubat <kubat.aiupov@gmail.com>
2023-02-25 21:57:07 +0100

    add mock data, add props to component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <h1>Simple App!</h1>
-        </header>
-        <p>Basic Component</p>
-        <ExpenseItem />
+      </header>
+      <p>Basic Component</p>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date.toISOString()} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './ExpenseItem.css'
-export default function ExpenseItem() {
 
-  const date = Date.now();
+export default function ExpenseItem(props) {
+
   return (
     <div className='expense-item'>
-      <h2>{date}</h2>
+      <h2>{props.date}</h2>
       <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   )
