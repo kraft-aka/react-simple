1ec27764246c720b7834e05cbf2745e108216ac6
kubat <kubat.aiupov@gmail.com>
2023-02-26 22:10:51 +0100

    create ExpenseDate Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,15 @@
-import React from 'react'
-import './ExpenseItem.css'
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-
-  // create three variables to hold different values from date props
-  const month = props.date.toLocaleString('en-GB', { month: 'long' })
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-  const year = props.date.getFullYear()
-
   return (
-    <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      <div className='expense-item__description'>
+    <div className="expense-item">
+    <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
-  )
+  );
 }
