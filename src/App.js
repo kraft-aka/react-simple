be62105704b45d85d2d243127b0eb8a66a66f2f5
kubat <kubat.aiupov@gmail.com>
2023-02-26 21:30:31 +0100

    edit date props in the component

--- src/App.js
+++ src/App.js
@@ -28,7 +28,29 @@ function App() {
         <h1>Simple App!</h1>
       </header>
       <p>Basic Component</p>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date.toISOString()} />
+      <div>
+        <h2>Expenses Tracker</h2>
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        ></ExpenseItem>
+      </div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,18 @@ import './ExpenseItem.css'
 
 export default function ExpenseItem(props) {
 
+  // create three variables to hold different values from date props
+  const month = props.date.toLocaleString('en-GB', { month: 'long' })
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+  const year = props.date.getFullYear()
+
   return (
     <div className='expense-item'>
-      <h2>{props.date}</h2>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
