3fba78722f093c8551e5a996078ce76f77bd40c5
kubat <kubat.aiupov@gmail.com>
2023-02-27 22:10:10 +0100

    add button to ExpenseItem component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,18 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
+
+  const clickHandler = () => {
+    console.log('clicked!')
+  }
   return (
     <Card className="expense-item">
     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-      </div>
+        <button onClick={clickHandler}>Change Title</button>
+        </div>
     </Card>
   );
 }
