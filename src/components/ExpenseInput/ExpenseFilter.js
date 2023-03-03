9f2293d966a0f837bace71f610bffcbadd9db8a4
kubat <kubat.aiupov@gmail.com>
2023-03-03 22:03:34 +0100

    fix code in ExpenseFilter

--- src/components/ExpenseInput/ExpenseFilter.js
+++ src/components/ExpenseInput/ExpenseFilter.js
@@ -7,15 +7,19 @@ export default function ExpenseFilter(props) {
     props.onExpenseFilter(e.target.value)
   }
 
-  return 
-    <div className='expenses-filter' >
-      <label for="filter-expenses"> Filter by year</label>
-      <select value={props.selected} id="filter" name="expenses-years" onChange={yearFilterHandler}>
-        <option value='2018'>2018</option>
-        <option value='2019'>2019</option>
-        <option value='2020'>2020</option>
-        <option value='2021'>2021</option>
-        <option value='2022'>2022</option>
-      </select>
+  return (
+    <div className='expenses-filter'>
+        <div className='expenses-filter__control'>
+          <label>Filter by year</label>
+          <select value={props.selected} onChange={yearFilterHandler}>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+          </select>
+        </div>
     </div>
+  )
 }
