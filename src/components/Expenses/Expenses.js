630af40a573db8199dd058122dbb25313350b114
kubat <kubat.aiupov@gmail.com>
2023-03-06 21:10:11 +0100

    map prop list dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,41 +1,29 @@
-import React, {useState} from 'react'
-import Card from '../UI/Card'
-import ExpenseFilter from '../ExpenseInput/ExpenseFilter'
-import ExpenseItem from './ExpenseItem'
-import './Expenses.css'
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseFilter from "../ExpenseInput/ExpenseFilter";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 export default function Expenses(props) {
-
-  const [filteredYear, setFilteredYear] = useState('2018')
+  const [filteredYear, setFilteredYear] = useState("2018");
   const expenseFilterHandler = (selectedYear) => {
-    setFilteredYear(selectedYear)
-  }
-  //console.log(props)
+    setFilteredYear(selectedYear);
+  };
   return (
     <div>
-    <Card className='expenses'>
-      <ExpenseFilter selected = {filteredYear} onExpenseFilter={expenseFilterHandler} />
-      <ExpenseItem
-      title={props.expenses[0].title}
-      amount={props.expenses[0].amount}
-      date={props.expenses[0].date}
-    />
-    <ExpenseItem
-      title={props.expenses[1].title}
-      amount={props.expenses[1].amount}
-      date={props.expenses[1].date}
-    />
-    <ExpenseItem
-      title={props.expenses[2].title}
-      amount={props.expenses[2].amount}
-      date={props.expenses[2].date}
-    />
-    <ExpenseItem
-      title={props.expenses[3].title}
-      amount={props.expenses[3].amount}
-      date={props.expenses[3].date}
-    />
-   </Card>
-   </div>
- )
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onExpenseFilter={expenseFilterHandler}
+        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 }
